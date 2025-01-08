1065ddcd8e2f2d63e5ca16a09c2aae616eb9075b
Monteiro Futila <jonquerfutila@gmail.com>
2025-01-08 17:03:04 +0100

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -19,7 +19,6 @@
     <meta property="og:title" content="{{ config('app.name', 'Pinkary') }} - Transforme cliques em dados valiosos" />
     <meta property="og:description"
         content="{{ config('app.name', 'Pinkary') }} - Transforme cliques em dados valiosos" />
-    <meta property="og:image" content="{{ asset('images/logo.png') }}" />
 
     <title>{{ config('app.name', 'Facade Link') }}</title>
 
